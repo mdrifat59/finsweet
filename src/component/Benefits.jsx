d560fe1602a1fdd5455949045cec34c819002591
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-25 12:43:00 +0600

    Benefit section responsive is done

--- src/component/Benefits.jsx
+++ src/component/Benefits.jsx
@@ -4,15 +4,15 @@ import BenefitCarousel from './carousel/BenefitCarousel'
 const Benefits = () => {
     return (
         <>
-            <section className='w-full py-32 bg-[#F5F2F0] '>
-                <div className='max-w-container mx-auto'>
+            <section className='w-full py-8 lg:py-16 xl:py-32 bg-[#F5F2F0] '>
+                <div className='w-full px-4 sm:px-8 lg:max-w-container lg:mx-auto'>
                     <div className='flex justify-center'>
                         <div className='text-center'>
-                            <label className='font-Roboto_Regular text-base text-[#161722] uppercase'>Watch and listen</label>
-                            <h2 className='w-[438px] font-Roboto_Bold text-5xl text-[#161722] leading-[64px] mt-5 uppercase'>THE benefits of joining our church</h2>
+                            <label className='font-Roboto_Regular text-sm md:text-base text-[#161722] uppercase'>Watch and listen</label>
+                            <h2 className='lg:w-[438px] font-Roboto_Bold text-2xl md:text-3xl lg:text-5xl text-[#161722] lg:leading-[64px] mt-3 lg:mt-5 uppercase'>THE benefits of joining our church</h2>
                         </div>
                     </div>
-                    <div className='w-full mt-16'>
+                    <div className='w-full mt-8 lg:mt-16'>
                         <BenefitCarousel />
                     </div>
                 </div>
--- src/component/carousel/BenefitCarousel.jsx
+++ src/component/carousel/BenefitCarousel.jsx
@@ -6,9 +6,9 @@ import "react-multi-carousel/lib/styles.css";
 const BenefitCarousel = () => {
     const responsive = {
         desktop: { breakpoint: { max: 3000, min: 1400 }, items: 4 },
-        sm_desktop: { breakpoint: { max: 1400, min: 1024 }, items: 4 },
-        tablet: { breakpoint: { max: 1024, min: 700 }, items: 2 },
-        sm: { breakpoint: { max: 700, min: 464 }, items: 1 },
+        sm_desktop: { breakpoint: { max: 1400, min: 1024 }, items: 3 },
+        tablet: { breakpoint: { max: 1024, min: 576 }, items: 2 },
+        sm: { breakpoint: { max: 576, min: 464 }, items: 1 },
         mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
     };
     return (
@@ -23,25 +23,27 @@ const BenefitCarousel = () => {
                         keyBoardControl={true}
                         showDots={false}
                         arrows={false}
+                    // centerMode={true}
                     >
                         {/* Example items with styling */}
-                        <div className={`w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit1.png')] flex items-end bg-cover`}>
+
+                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit1.png')] flex items-end bg-cover`}>
                             <div>
                                 <h5 className='font-Roboto_Bold text-2xl text-[#FFFFFF]'>WATCH AND LISTEN TO OUR SERMONS</h5>
                                 <p className='font-Roboto_Regular text-base text-[#c8c8c8] mt-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
                             </div>
                         </div>
-                        <div className={`w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit2.png')] flex items-end bg-cover`}>
+                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit2.png')] flex items-end bg-cover`}>
                             <div>
                                 <h5 className='font-Roboto_Bold text-2xl text-[#FFFFFF]'>WATCH AND LISTEN TO OUR SERMONS</h5>
                             </div>
                         </div>
-                        <div className={`w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit3.png')] flex items-end bg-cover`}>
+                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit3.png')] flex items-end bg-cover`}>
                             <div>
                                 <h5 className='font-Roboto_Bold text-2xl text-[#FFFFFF]'>WATCH AND LISTEN TO OUR SERMONS</h5>
                             </div>
                         </div>
-                        <div className={`w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit4.png')] flex items-end bg-cover`}>
+                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[url('/public/img/benefit4.png')] flex items-end bg-cover`}>
                             <div>
                                 <h5 className='font-Roboto_Bold text-2xl text-[#FFFFFF]'>WATCH AND LISTEN TO OUR SERMONS</h5>
                             </div>

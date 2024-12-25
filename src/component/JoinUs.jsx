754e6ae787cc524bc1fdbd85efa3e29451838e2b
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-25 12:53:33 +0600

    Join us section is responsive done

--- src/component/JoinUs.jsx
+++ src/component/JoinUs.jsx
@@ -7,16 +7,16 @@ import { GoArrowRight } from 'react-icons/go'
 const JoinUs = () => {
     return (
         <>
-            <section className='w-full py-32 bg-white'>
-                <div className='max-w-container mx-auto'>
+            <section className='w-full py-8 lg:py-16 xl:py-32 bg-white'>
+                <div className='w-full px-4 sm:px-8 lg:max-w-container lg:mx-auto'>
                     <div className='flex justify-center'>
                         <div className='text-center'>
-                            <label className='font-Roboto_Regular text-base text-[#161722] uppercase'>Upcoming SERMONS</label>
-                            <h2 className='w-[554px] font-Roboto_Bold text-5xl text-[#161722] leading-[64px] mt-5 uppercase'>join us and become part of something great</h2>
+                            <label className='font-Roboto_Regular text-xs md:sm lg:text-base text-[#161722] uppercase'>Upcoming SERMONS</label>
+                            <h2 className='w-[350px] lg:w-[554px] font-Roboto_Bold text-2xl md:text-3xl lg:text-5xl text-[#161722] lg:leading-[64px] mt-5 uppercase'>join us and become part of something great</h2>
                         </div>
                     </div>
-                    <div className='flex mt-16'>
-                        <div className='w-[435px] bg-[#FFF5EB] py-[61px] px-[90px] relative'>
+                    <div className='lg:flex mt-16'>
+                        <div className='w-full lg:w-[435px] bg-[#FFF5EB] py-[61px] mb-5 lg:mb-0 px-8 lg:px-[90px] relative'>
                             <div className='absolute top-8 right-8 flex flex-col justify-center items-center'>
                                 <h5 className='font-Roboto_Bold text-2xl text-[#161722] leading-3'>20</h5>
                                 <label className='font-Roboto_Regular text-base text-[#161722]'>JULY</label>
@@ -40,14 +40,14 @@ const JoinUs = () => {
                                     <p className='w-[184px] font-Roboto_Regular text-base text-[#161722]'>No 233 Main St. New York, United States</p>
                                 </div>
                             </div>
-                            <button className='font-Roboto_Regular py-6 px-16 text-base text-[#FFD2A4] bg-[#161722] uppercase rounded-xl'>Register</button>
+                            <button className='font-Roboto_Regular py-3 lg:py-6 px-8 lg:px-16 text-base text-[#FFD2A4] bg-[#161722] uppercase rounded-xl'>Register</button>
                         </div>
-                        <div className='w-[845px] '>
+                        <div className='w-full lg:w-[845px] '>
                             <img src={join} className='w-full h-full object-cover' alt="" />
                         </div>
                     </div>
                     <div className='flex justify-end items-center gap-1 mt-[50px]'>
-                        <label className='font-Roboto_Regular text-2xl text-[#161722]'>View all Sermons</label>
+                        <label className='font-Roboto_Regular text-base lg:text-2xl text-[#161722]'>View all Sermons</label>
                         <GoArrowRight className='w-[24px] text-[#161722]' />
                     </div>
                 </div>

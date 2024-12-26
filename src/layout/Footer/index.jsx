c4a9bc5baafc877ba2be1fc5badb471d74e13b07
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-26 20:52:00 +0600

    Footer section responsive is done

--- src/layout/Footer/index.jsx
+++ src/layout/Footer/index.jsx
@@ -6,21 +6,21 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
     return (
         <>
-            <footer className='w-full py-16 bg-[#161722]'>
-                <div className='max-w-container mx-auto flex'>
-                    <div className='mr-[259px]'>
-                        <div>
+            <footer className='w-full py-8 lg:py-16 xl:py-32 bg-[#161722]'>
+                <div className='w-full px-4 sm:px-8 lg:max-w-container lg:mx-auto flex flex-wrap xl:flex-nowrap xl:justify-start justify-around  gap-10 xl:gap-0'>
+                    <div className='lg:mr-[200px]'>
+                        <div className='flex flex-col'>
                             <VactorIconFooter className='mb-4 bg-gradient-to-r from-[#f2b890] to-[#fdd2a1] inline-block text-transparent bg-clip-text  ' />
                             <caption className='w-[148px] font-Roboto_Regular text-xs text-[#FFFFFF]'>© Copyright Finsweet 2022</caption>
                         </div>
-                        <div className='flex flex-col gap-4 pt-8'>
+                        <div className='flex flex-col gap-4 pt-8  '>
                             <caption className='w-[140px] font-Roboto_Regular text-xs text-[#FFFFFF]'>(480) 555-0103</caption>
                             <caption className='w-[140px] font-Roboto_Regular text-xs text-[#FFFFFF]'>4517 Washington Ave. </caption>
                             <caption className='w-[140px] font-Roboto_Regular text-xs text-[#FFFFFF]'>finsweet@example.com</caption>
                         </div>
                     </div>
 
-                    <div className='mr-[72px]'>
+                    <div className='lg:mr-[72px] '>
                         <p className='font-Roboto_Regular text-base text-[#FFFFFF] mb-[20px]'>Quicklinks</p>
                         <div className='flex flex-col gap-6 '>
                             <caption className='font-Roboto_Regular text-xs text-[#ccccce] uppercase'>About us</caption>
@@ -30,7 +30,7 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    <div className='mr-[105px]'>
+                    <div className='lg:mr-[105px]   '>
                         <p className='font-Roboto_Regular text-base text-[#FFFFFF]'>Connect</p>
                         <div className='flex gap-5 mt-5'>
                             <Link>
@@ -45,9 +45,9 @@ const Footer = () => {
                         </div>
                     </div>
                     <div>
-                        <h4 className='w-[380px] font-Roboto_Bold text-[32px] text-[#FFFFFF] uppercase'>Subscribe to get Latest Updates and News</h4>
+                        <h4 className='xl:w-[380px] font-Roboto_Bold text-[32px] text-[#FFFFFF] uppercase'>Subscribe to get Latest Updates and News</h4>
                         <div className='relative mt-8'>
-                            <input type="email" className='w-[512px]  p-5 rounded-xl bg-transparent border border-[#393a42] text-white outline-none placeholder:font-Roboto_Regular placeholder:text-base placeholder:text-[#d0d1d3]' placeholder='Yourmail@gmail.com' />
+                            <input type="email" className='w-full 2xl:w-[512px]  p-5 rounded-xl bg-transparent border border-[#393a42] text-white outline-none placeholder:font-Roboto_Regular placeholder:text-base placeholder:text-[#d0d1d3]' placeholder='Yourmail@gmail.com' />
                             <button className='absolute top-0 right-0 font-Roboto_Regular text-base text-[#1C1D28] py-5 px-16 bg-[#FFD2A4] rounded-xl uppercase'>Subscribe</button>
                         </div>
                     </div>

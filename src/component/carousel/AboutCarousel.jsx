d6b00ffa839269c2679c895a1c85c68ce323e4c0
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-29 20:38:43 +0600

    About page AboutCarousel some edit

--- src/component/carousel/AboutCarousel.jsx
+++ src/component/carousel/AboutCarousel.jsx
@@ -12,10 +12,10 @@ import { FacebookIcons, LinkdinIcons, TwitterIcons } from '../../svg/Icons';
 const AboutCarousel = () => {
     const responsive = {
         desktop: { breakpoint: { max: 3000, min: 1400 }, items: 4 },
-        sm_desktop: { breakpoint: { max: 1400, min: 1024 }, items: 3 },
-        tablet: { breakpoint: { max: 1024, min: 576 }, items: 2 },
-        sm: { breakpoint: { max: 576, min: 464 }, items: 1 },
-        mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+        sm_desktop: { breakpoint: { max: 1400, min: 1200 }, items: 4 },
+        tablet: { breakpoint: { max: 1200, min: 992 }, items: 3 },
+        sm: { breakpoint: { max: 992, min: 768 }, items: 2 },
+        mobile: { breakpoint: { max: 768, min: 0 }, items: 1 }
     };
     return (
         <>
@@ -31,80 +31,89 @@ const AboutCarousel = () => {
                         arrows={false}
                     >
                         {/* Example items with styling */}
-
-                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
-                            <div className='flex flex-col justify-center items-center'>
-                                <img src={team_1} alt="" />
-                                <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'> Kim Bowen</h5>
-                                <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
-                                <div className='flex gap-5'>
-                                    <Link >
-                                        <FacebookIcons />
-                                    </Link>
-                                    <Link>
-                                        <TwitterIcons />
-                                    </Link>
-                                    <Link>
-                                        <LinkdinIcons />
-                                    </Link>
+                        <div className='flex justify-center items-center'>
+                            <div className={`w-[330px]  md:w-[330px] lg:w-[280px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
+                                <div className='flex flex-col justify-center items-center'>
+                                    <img src={team_1} alt="" />
+                                    <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'> Kim Bowen</h5>
+                                    <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
+                                    <div className='flex gap-5'>
+                                        <Link >
+                                            <FacebookIcons />
+                                        </Link>
+                                        <Link>
+                                            <TwitterIcons />
+                                        </Link>
+                                        <Link>
+                                            <LinkdinIcons />
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
+
                         {/* second slides */}
-                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
-                            <div className='flex flex-col justify-center items-center'>
-                                <img src={team_2} alt="" />
-                                <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'>Danielle Watkins</h5>
-                                <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
-                                <div className='flex gap-5'>
-                                    <Link >
-                                        <FacebookIcons />
-                                    </Link>
-                                    <Link>
-                                        <TwitterIcons />
-                                    </Link>
-                                    <Link>
-                                        <LinkdinIcons />
-                                    </Link>
+                        <div className="flex justify-center items-center">
+                            <div className={`w-[330px] md:w-[330px] lg:w-[280px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
+                                <div className='flex flex-col justify-center items-center'>
+                                    <img src={team_2} alt="" />
+                                    <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'>Danielle Watkins</h5>
+                                    <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
+                                    <div className='flex gap-5'>
+                                        <Link >
+                                            <FacebookIcons />
+                                        </Link>
+                                        <Link>
+                                            <TwitterIcons />
+                                        </Link>
+                                        <Link>
+                                            <LinkdinIcons />
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
                         {/* Third slider */}
-                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
-                            <div className='flex flex-col justify-center items-center'>
-                                <img src={team_3} alt="" />
-                                <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'>Naomi Craig</h5>
-                                <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
-                                <div className='flex gap-5'>
-                                    <Link >
-                                        <FacebookIcons />
-                                    </Link>
-                                    <Link>
-                                        <TwitterIcons />
-                                    </Link>
-                                    <Link>
-                                        <LinkdinIcons />
-                                    </Link>
+                        <div className="flex justify-center items-center">
+                            <div className={`w-[330px] md:w-[330px] lg:w-[280px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
+                                <div className='flex flex-col justify-center items-center'>
+                                    <img src={team_3} alt="" />
+                                    <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'>Naomi Craig</h5>
+                                    <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
+                                    <div className='flex gap-5'>
+                                        <Link >
+                                            <FacebookIcons />
+                                        </Link>
+                                        <Link>
+                                            <TwitterIcons />
+                                        </Link>
+                                        <Link>
+                                            <LinkdinIcons />
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className={`w-full sm:w-[240px] md:w-[330px] lg:w-[302px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
-                            <div className='flex flex-col justify-center items-center'>
-                                <img src={team_4} alt="" />
-                                <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'>Santos Payne</h5>
-                                <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
-                                <div className='flex gap-5'>
-                                    <Link >
-                                        <FacebookIcons />
-                                    </Link>
-                                    <Link>
-                                        <TwitterIcons />
-                                    </Link>
-                                    <Link>
-                                        <LinkdinIcons />
-                                    </Link>
+                        {/* four slider */}
+                        <div className="flex justify-center items-center">
+                            <div className={`w-[330px] md:w-[330px] lg:w-[280px] h-[384px] p-8 bg-[#F5F2F0]   flex justify-center items-center`}>
+                                <div className='flex flex-col justify-center items-center'>
+                                    <img src={team_4} alt="" />
+                                    <h5 className='font-Roboto_Bold text-2xl text-[#161722] mt-6'>Santos Payne</h5>
+                                    <p className='font-Roboto_Regular text-base text-[#595960] my-4'>Pastor, Church</p>
+                                    <div className='flex gap-5'>
+                                        <Link >
+                                            <FacebookIcons />
+                                        </Link>
+                                        <Link>
+                                            <TwitterIcons />
+                                        </Link>
+                                        <Link>
+                                            <LinkdinIcons />
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>

692c1995eedf68dac8cada0501f3061d6ce1dd43
mdrifat59 <mdrifatulislam59@gmail.com>
2024-12-29 15:13:03 +0600

    About page responsive done

--- src/component/About/Banefits.jsx
+++ src/component/About/Banefits.jsx
@@ -9,49 +9,49 @@ const Banefits = () => {
         <>
             <div className="py-8 lg:py-16 xl:py-32 bg-white">
                 <div className='w-full px-4 sm:px-8 lg:max-w-container lg:mx-auto'>
-                    <div className="flex flex-row justify-center">
+                    <div className="flex flex-row justify-center xl:mb-10">
                         <div>
                             <h4 className='font-Roboto_Regular text-xs md:text-base text-[#161722] text-center'>BENEFITS </h4>
-                            <h2 className='w-[440px] font-Roboto_Bold text-2xl md:text-3xl lg:text-5xl text-[#161722] uppercase text-center mb-8 mt-4'> THE benefits of joining our church</h2>
+                            <h2 className='lg:w-[440px] font-Roboto_Bold text-2xl md:text-3xl lg:text-5xl text-[#161722] uppercase text-center mb-8 mt-4'> THE benefits of joining our church</h2>
                         </div>
                     </div>
                     {/* contente */}
                     <div className="grid gap-y-[96px]">
 
-                        <div className='flex justify-center'>
-                            <div className="flex flex-col justify-center">
-                                <h4 className='font-Roboto_Bold text-[32px] uppercase text-[#161722] '>find fulfillment and joy</h4>
-                                <p className='w-[500px] font-Roboto_Regular text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                        <div className='flex flex-col md:flex-row justify-center gap-5'>
+                            <div className="flex flex-col  justify-center">
+                                <h4 className='font-Roboto_Bold text-xl sm:text-2xl lg:text-[32px] mb-3 uppercase text-[#161722] '>find fulfillment and joy</h4>
+                                <p className=' md:w-[350px] lg:w-[500px] font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
                             </div>
                             <div>
                                 <img src={Banefit_1} alt="" />
                             </div>
                         </div>
-                        <div className='flex justify-center'>
-                            <div className='mr-12'>
+                        <div className='flex flex-col-reverse md:flex-row justify-center gap-5'>
+                            <div className='lg:mr-12'>
                                 <img src={Banefit_2} alt="" />
                             </div>
                             <div className="flex flex-col justify-center">
-                                <h4 className='font-Roboto_Bold text-[32px] uppercase text-[#161722] '>shared values</h4>
-                                <p className='w-[500px] font-Roboto_Regular text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                                <h4 className='font-Roboto_Bold text-xl sm:text-2xl lg:text-[32px] mb-3 uppercase text-[#161722] '>shared values</h4>
+                                <p className='md:w-[350px] lg:w-[500px] font-Roboto_Regular  text-xs md:text-sm lg:text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
                             </div>
                         </div>
-                        <div className='flex justify-center'>
-                            <div className="flex flex-col justify-center">
-                                <h4 className='font-Roboto_Bold text-[32px] uppercase text-[#161722] '>charity events</h4>
-                                <p className='w-[500px] font-Roboto_Regular text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                        <div className='flex flex-col md:flex-row justify-center gap-5'>
+                            <div className="flex flex-col  justify-center">
+                                <h4 className='font-Roboto_Bold text-xl sm:text-2xl lg:text-[32px] mb-3 uppercase text-[#161722] '>charity events</h4>
+                                <p className=' md:w-[350px] lg:w-[500px] font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
                             </div>
                             <div>
                                 <img src={Banefit_3} alt="" />
                             </div>
                         </div>
-                        <div className='flex justify-center'>
-                            <div className='mr-12'>
+                        <div className='flex flex-col-reverse md:flex-row justify-center gap-5'>
+                            <div className='lg:mr-12'>
                                 <img src={Banefit_4} alt="" />
                             </div>
                             <div className="flex flex-col justify-center">
-                                <h4 className='font-Roboto_Bold text-[32px] uppercase text-[#161722] '>All are welcome</h4>
-                                <p className='w-[500px] font-Roboto_Regular text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+                                <h4 className='font-Roboto_Bold text-xl sm:text-2xl lg:text-[32px] mb-3 uppercase text-[#161722]'>All are welcome</h4>
+                                <p className='md:w-[350px] lg:w-[500px] font-Roboto_Regular  text-xs md:text-sm lg:text-base text-[#161722] leading-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
                             </div>
                         </div>
                     </div>
--- src/component/About/Compassion.jsx
+++ src/component/About/Compassion.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import comp1 from '/public/img/comp1.png'
 import comp2 from '/public/img/comp2.png'
-import comp3 from '/public/img/comp3.png'
-import { GoArrowRight } from 'react-icons/go'
+import comp3 from '/public/img/comp3.png' 
 
 const Compassion = () => {
     return (
@@ -13,7 +12,7 @@ const Compassion = () => {
                         <div>
                             <h4 className='font-Roboto_Regular text-xs md:text-base text-[#161722] text-center'>Welcome to our CHURCH</h4>
                             <h2 className='font-Roboto_Bold text-2xl md:text-3xl lg:text-5xl text-[#161722] uppercase text-center mb-8 mt-4'>Love and compassion</h2>
-                            <p className='w-[370px] md:w-[520px] lg:w-[846px] font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722] text-center '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>                          
+                            <p className='w-[370px] md:w-[520px] lg:w-[846px] font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722] text-center '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
                         </div>
                     </div>
                     <div className='xl:w-[1024px] mx-auto gap-2 '>
@@ -29,18 +28,18 @@ const Compassion = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='flex justify-center pt-8 lg:pt-16 gap-6'>
+                    <div className='w-full flex flex-col md:flex-row justify-center items-center pt-10 lg:pt-16 gap-6'>
                         <div className="">
-                            <label className='font-Roboto_Regular text-base text-[#161722]'> OUR MISSIOn & Vision </label>
-                            <h4 className='w-[371px] my-4 font-Roboto_Bold  text-[32px] text-[#161722] leading-[44px]'>STRIVING FOR A BETTER TOMORROW </h4>
-                            <p className='w-[372px] my-4 font-Roboto_Regular text-base text-[#161722] leading-6'>
+                            <label className='font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722]'> OUR MISSIOn & Vision </label>
+                            <h4 className='w-[300px] lg:w-[371px] sm:my-4 font-Roboto_Bold text-xl sm:text-2xl lg:text-[32px] text-[#161722] lg:leading-[44px]'>STRIVING FOR A BETTER TOMORROW </h4>
+                            <p className='w-[300px] lg:w-[372px] lg:my-4 font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722] leading-6'>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
                             </p>
                         </div>
                         <div className="">
-                            <label className='font-Roboto_Regular text-base text-[#161722]'> WHAT WE DO </label>
-                            <h4 className='w-[371px] my-4 font-Roboto_Bold  text-[32px] text-[#161722] leading-[44px]'>BRINgING PEACE AND JOY TO THE WORLD </h4>
-                            <p className='w-[372px] my-4 font-Roboto_Regular text-base text-[#161722] leading-6'>
+                            <label className='font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722]'> WHAT WE DO </label>
+                            <h4 className='w-[300px] lg:w-[371px] sm:my-4 font-Roboto_Bold text-xl sm:text-2xl lg:text-[32px] text-[#161722] lg:leading-[44px]'>BRINgING PEACE AND JOY TO THE WORLD </h4>
+                            <p className='w-[300px] lg:w-[372px] lg:my-4 font-Roboto_Regular text-xs md:text-sm lg:text-base text-[#161722] leading-6'>
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
                             </p>
                         </div>
